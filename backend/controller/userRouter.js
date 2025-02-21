e25acac513077decc4df65365faa1585ec92e8b2
mukesh vantakula <durgasaimukeshvantakula5764@gmail.com>
2025-02-21 11:50:41 +0530

    updated

--- backend/controller/userRouter.js
+++ backend/controller/userRouter.js
@@ -40,29 +40,30 @@ module.exports = router;
 
 // User Login Route
 router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+      const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
+      const user = await User.findOne({ email });
+      if (!user) {
+          return res.status(400).json({ message: 'Invalid credentials' });
+      }
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(403).json({ message: 'Invalid credentials' });
-        }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+          return res.status(403).json({ message: 'Invalid credentials' });
+      }
 
-        const token = jwt.sign(
-            { userId: user._id, email: user.email },
-            process.env.SECRET_KEY,
-            { expiresIn: '1h' }
-        );
+      const token = jwt.sign(
+          { userId: user._id, email: user.email },
+          process.env.SECRET_KEY,
+          { expiresIn: '1h' }
+      );
 
-        res.status(200).json({ message: 'Login successful', token });
-    } catch (err) {
-        res.status(500).json({ message: 'Server Error' });
-    }
+      res.status(200).json({ message: 'Login successful', token });
+  } catch (err) {
+    console.log
+      res.status(500).json({ message: 'Server Error' });
+  }
 });
 
 module.exports = router;
